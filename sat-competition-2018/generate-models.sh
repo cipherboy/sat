5c8fc622391d6c8581b9435af090bd57607ddb57
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-04-10 12:27:32 -0500

    Minor changes to generation.
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- sat-competition-2018/generate-models.sh
+++ sat-competition-2018/generate-models.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+rm -rf models
+mkdir -p models
+
 python3 sha3-chi-order.py
 python3 sha3-theta-order.py
 
 # See md4-families.args for possible argument values which produce SAT/UNSAT
 # with estimated timings.
 
-# Else for a series of benchmarks, run something like the following:
-# (many easy SAT/UNSAT):
-python3 md4-families.py 28 4
+# Else for a large series of benchmarks, run something like the following:
+# (has many easy SAT/UNSAT mixed in with harder instances)
+
+# python3 md4-families.py 28 4
